7e9661261244f35a790921d2952fc33af85defd5
tim@5f1bb9e6-f381-11dc-ac94-59e6f598b727 <tim@5f1bb9e6-f381-11dc-ac94-59e6f598b727>
2008-07-28 19:39:04 +0000

    re #19 - created method stubs for all messages identified in CapturedMessageUsage page

--- src/message/PsaiMessageRedirector.h
+++ src/message/PsaiMessageRedirector.h
@@ -11,6 +11,25 @@
 class psClientNetSubscriber;
 class MsgHandler;
 class psChatMessage;
+class psPlaySoundMessage;
+class psSoundEventMessage;
+class psPersistItem;
+class psPersistActor;
+class psPersistActionLocation;
+class psRemoveObject;
+class psDRMessage;
+class psStatDRMessage;
+class psCombatEventMessage;
+class psModeMessage;
+class psMoveLockMessage;
+class psNewSectorMessage;
+class psEffectMessage;
+class psStopEffectMessage;
+class psSpellCastMessage;
+class psSpellCancelMessage;
+class psStatsMessage;
+class psSystemMessage;
+class psWeatherMessage;
 
 class PsaiMessageRedirector: public psClientNetSubscriber
 {
@@ -24,6 +43,42 @@ class PsaiMessageRedirector: public psClientNetSubscriber
 		void setupSubscriptions(MsgHandler* messageHandler);
 
 		void handleChatMessage(psChatMessage& msg);
+
+		void handlePlaySoundMessage(psPlaySoundMessage& msg);
+
+		void handleSoundEventMessage(psSoundEventMessage& msg);
+
+		void handlePersistItemMessage(psPersistItem& msg);
+
+		void handlePersistActorMessage(psPersistActor& msg);
+
+		void handlePersistActionLocationMessage(psPersistActionLocation& msg);
+
+		void handleRemoveObjectMessage(psRemoveObject& msg);
+
+		void handleDeadReckonMessage(psDRMessage& msg);
+
+		void handleStatDeadReckonMessage(psStatDRMessage& msg);
+
+		void handleCombatEventMessage(psCombatEventMessage& msg);
+
+		void handleModeMessage(psModeMessage& msg);
+
+		void handleMoveLockMessage(psMoveLockMessage& msg);
+
+		void handleNewSectorMessage(psNewSectorMessage& msg);
+
+		void handleEffectMessage(psEffectMessage& msg);
+
+		void handleStopEffectMessage(psStopEffectMessage& msg);
+
+		void handleSpellCastMessage(psSpellCastMessage& msg);
+
+		void handleSpellCancelMessage(psSpellCancelMessage& msg);
+
+		void handleStatsMessage(psStatsMessage& msg);
+
+		void handleWeatherMessage(psWeatherMessage& msg);
 };
 
 #endif /* PSAIMESSAGEREDIRECTOR_H_ */
