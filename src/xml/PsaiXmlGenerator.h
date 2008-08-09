cb1d4feddd8670f33333f6bd8c7ce2966c53b139
tim@5f1bb9e6-f381-11dc-ac94-59e6f598b727 <tim@5f1bb9e6-f381-11dc-ac94-59e6f598b727>
2008-08-09 07:26:09 +0000

    re #22 - added stubs for all messages currently registered in the redirector

--- src/xml/PsaiXmlGenerator.h
+++ src/xml/PsaiXmlGenerator.h
@@ -57,17 +57,78 @@ class PsaiXmlGenerator
 	private:
 
 		std::string toXml(psChatMessage& msg);
+
 		std::string toXml(psPlaySoundMessage& msg);
+
 		std::string toXml(psSoundEventMessage& msg);
+
 		std::string toXml(psPersistItem& msg);
+
 		std::string toXml(psPersistActor& msg);
+
 		std::string toXml(psPersistActionLocation& msg);
+
+		std::string toXml(psRemoveObject& msg);
+
+		std::string toXml(psDRMessage& msg);
+
+		std::string toXml(psStatDRMessage& msg);
+
+		std::string toXml(psCombatEventMessage& msg);
+
+		std::string toXml(psModeMessage& msg);
+
+		std::string toXml(psMoveLockMessage& msg);
+
+		std::string toXml(psNewSectorMessage& msg);
+
+		std::string toXml(psEffectMessage& msg);
+
+		std::string toXml(psStopEffectMessage& msg);
+
+		std::string toXml(psSpellCastMessage& msg);
+
+		std::string toXml(psSpellCancelMessage& msg);
+
+		std::string toXml(psStatsMessage& msg);
+
+		std::string toXml(psSystemMessage& msg);
+
+		std::string toXml(psWeatherMessage& msg);
+
+		std::string toXml(psCharacterDetailsMessage& msg);
+
+		std::string toXml(psGUIInventoryMessage& msg);
+
+		std::string toXml(psGUIActiveMagicMessage& msg);
+
+		std::string toXml(psGUIInteractMessage& msg);
+
+		std::string toXml(psGUIMerchantMessage& msg);
+
+		std::string toXml(psGUISkillMessage& msg);
+
+		std::string toXml(psGUITargetUpdateMessage& msg);
+
+		std::string toXml(psLootMessage& msg);
+
+		std::string toXml(psQuestListMessage& msg);
+
+		std::string toXml(psQuestRewardMessage& msg);
+
+		std::string toXml(psUpdateObjectNameMessage& msg);
+
+		std::string toXml(psViewItemDescription& msg);
+
+		std::string toXml(psViewItemUpdate& msg);
+
+		std::string toXml(psEquipmentMessage& msg);
 
 		static const char* XML_HEADER;
 		static const char* PSAI_NAMESPACE;
 };
 
-static const char* XML_HEADER="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-static const char* PSAI_NAMESPACE="xmlns=\"http://www.psaidoesnt-exist.org/psaiXmlMessages\"";
+static const char* XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+static const char* PSAI_NAMESPACE = "xmlns=\"http://www.psaidoesnt-exist.org/psaiXmlMessages\"";
 
 #endif /* PSAIXMLGENERATOR_H_ */
